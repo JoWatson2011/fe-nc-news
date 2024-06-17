7b143f67c3dc72f8a6a08862355c0148f9411e48
jowatson2011 <jowatson@protonmail.com>
2024-06-17 15:32:22 +0100

    Implemented get request for an individual article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-const Article = ({article, setArticle}) => {
+import { getRequest } from "../utils/api";
+const Article = ({ article, setArticle }) => {
   const { article_id } = useParams();
+
+  console.log(article_id);
+  useEffect(() => {
+    getRequest(`/api/articles/${article_id}`, { params: { article_id } }).then(
+      ({ article }) => {
+        setArticle(article);
+      }
+    );
+  }, [article_id]);
+  
   return <p>{article_id}</p>;
 };
 export default Article;
