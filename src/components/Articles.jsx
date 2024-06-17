97b18c904c749102d98ddac209ab986d43d47af4
jowatson2011 <jowatson@protonmail.com>
2024-06-17 14:29:58 +0100

    Generated ArticleCards for each articles based on GET request in useEffect block

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,2 +1,21 @@
-const Articles = () => {};
+import { getRequest } from "../utils/api";
+import { useEffect } from "react";
+import ArticleCard from "./ArticleCard";
+const Articles = ({ listArticles, setlistArticles }) => {
+  useEffect(() => {
+    getRequest("/api/articles")
+      .then(({ articles }) => {
+        setlistArticles(articles);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className=" grid-flow-col  space-y-8">
+      {listArticles.map((article) => {
+        return <ArticleCard article={article} />;
+      })}
+    </div>
+  );
+};
 export default Articles;
--- src/components/ArticlesContainer.jsx
+++ src/components/ArticlesContainer.jsx
@@ -3,11 +3,21 @@ import Article from "./Article";
 import Home from "./Home";
 import PostArticle from "./PostArticle";
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 const ArticlesContainer = () => {
+  const [listArticles, setlistArticles] = useState([]);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/articles/:topic?" element={<Articles />} />
+      <Route
+        path="/articles/:topic?"
+        element={
+          <Articles
+            listArticles={listArticles}
+            setlistArticles={setlistArticles}
+          />
+        }
+      />
       <Route path="/articles/:article_id" element={<Article />} />
       <Route path="/articles/post" element={<PostArticle />} />
     </Routes>
