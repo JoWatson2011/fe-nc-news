57dd00eb7b1fbf294b423f54c59802a6748be4e9
jowatson2011 <jowatson@protonmail.com>
2024-06-17 15:52:56 +0100

    Added divider to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
+import * as React from "react";
+import Divider from "@mui/material/Divider";
+
 const Header = () => {
   return (
-    <h1 className = " font-mono text-[70px] text-red-700">nc news</h1>
-    
+    <div className=" mb-10">
+      <h1 className=" font-mono text-[70px] text-red-700">nc news</h1>
+      <div
+        className="grid grid-cols-5
+      ]"
+      >
+        <hr className=" border-red-400 border-4 col-span-4 mr-10"></hr>
+        <p className="grid col-span-1">MENU</p>
+      </div>
+    </div>
   );
 };
 
