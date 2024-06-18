9a31554f5506399c93282b18109ecdeee362b0dd
jowatson2011 <jowatson@protonmail.com>
2024-06-18 11:33:47 +0100

    Added tests for NavButton

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,6 +1,10 @@
 const NavButton = ({ buttonText }) => {
   return (
-    <button type="button" className=" p-2 bg-red-50 pd- rounded-full">
+    <button
+      data-cy="button"
+      type="button"
+      className=" p-2 bg-red-50 pd- rounded-full"
+    >
       {buttonText}
     </button>
   );
