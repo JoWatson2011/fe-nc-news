edc2e93739da2ac8c5343f7814614b8d84ea691c
jowatson2011 <jowatson@protonmail.com>
2024-06-18 16:13:54 +0100

    Added optimistic rendering for  vote buttons

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,16 +1,47 @@
 import { useEffect } from "react";
 const LikeButton = ({ postWithVotes, setFunction }) => {
   let displayText = postWithVotes.votes;
-	
-}
+
+  const postType = Object.keys(postWithVotes).includes("comment_id")
+    ? "comment"
+    : "article";
+  const postId = postWithVotes[`${postType}_id`];
+
+
+  const patchVote = (postId, value) => {
+    setFunction((currPosts) => {
+      console.log(currPosts);
+      if (!Array.isArray(currPosts) & (postType === "article")) {
+        return { ...currPosts, votes: currPosts.votes + value };
+      }
+
+      return currPosts.map((post) => {
+        if (post[`${postType}_id`] === postId) {
+          return { ...post, votes: post.votes + value };
+        }
+        return post;
+      });
+    });
+
+  };
 
   return (
     <div className="flex gap-x-2 items-center">
-      <button className="w-8 h-8 font-bold text-white bg-orange-800 rounded-full  hover:bg-red-500 active:bg-red-300">
+      <button
+        className="w-8 h-8 font-bold text-white bg-orange-800 rounded-full  hover:bg-red-500 active:bg-red-300"
+        onClick={() => {
+          patchVote(postId, -1);
+        }}
+      >
         -1
       </button>
       <p className=" text-xl font-bold">{displayText}</p>
-      <button className="w-8 h-8 font-bold text-white bg-green-950 rounded-full  hover:bg-green-700 active:bg-green-300">
+      <button
+        className="w-8 h-8 font-bold text-white bg-green-950 rounded-full  hover:bg-green-700 active:bg-green-300"
+        onClick={() => {
+          patchVote(postId, 1);
+        }}
+      >
         +1
       </button>
     </div>
