54eb135a90559ee61278638b36ccdd46f0e17242
jowatson2011 <jowatson@protonmail.com>
2024-06-18 14:26:51 +0100

    Added condition Loading component to Comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,22 +1,28 @@
 import { useState, useEffect } from "react";
 import { getRequest } from "../utils/api";
 import CommentCard from "./CommentCards";
+import Loading from "./Loading";
 const Comments = ({ article_id }) => {
   const [comments, setComments] = useState([]);
+  const [commentsLoading, setCommentsLoading] = useState(true);
   useEffect(() => {
     getRequest(`/api/articles/${article_id}/comments`, {
       params: { article_id },
     }).then(({ comments }) => {
       setComments(comments);
+      setCommentsLoading(false);
     });
-  });
+  }, []);
 
   return (
     <div>
       <h3>Comments</h3>
-      {comments.map((comment) => {
-        return <CommentCard comment={comment} />;
-      })}
+      {commentsLoading ? <Loading /> : null}
+      <div id="comment-cards">
+        {comments.map((comment) => {
+          return <CommentCard comment={comment} />;
+        })}
+      </div>
     </div>
   );
 };
