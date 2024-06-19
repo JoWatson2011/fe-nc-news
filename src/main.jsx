fda4ad948182b2a327959597f697841e1f8a4d23
jowatson2011 <jowatson@protonmail.com>
2024-06-19 10:40:24 +0100

    Added context to stored loggid in user

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./output.css";
+import { UserProvider } from "./contexts/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <UserProvider>
     <App />
-  </React.StrictMode>
+  </UserProvider>
 );
