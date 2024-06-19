7965d2c1a31d03ae915d4db5c6a88d912f935be3
jowatson2011 <jowatson@protonmail.com>
2024-06-19 10:12:33 +0100

    Added reative NavMenu in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,26 @@
-import * as React from "react";
-import Divider from "@mui/material/Divider";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import NavMenu from "./NavMenu";
 
 const Header = () => {
+  
   return (
     <div className=" mb-10">
-      <h1 className=" font-mono text-[70px] text-red-700">nc news</h1>
+      <Link to="/">
+        <h1 className=" font-mono text-[70px] text-red-700">nc news</h1>
+      </Link>
       <div
-        className="grid grid-cols-5
+        className="grid grid-cols-5 items-center
       ]"
       >
         <hr className=" border-red-700 border-2 col-span-4 mr-10"></hr>
-        <p className="grid col-span-1 text-red-700">MENU</p>
+        {/* <button
+          className="grid col-span-1 text-red-700"
+          onClick={toggleMenu(!menuOpen)}
+        >
+          Menu
+        </button> */}
+        <NavMenu />
       </div>
     </div>
   );
