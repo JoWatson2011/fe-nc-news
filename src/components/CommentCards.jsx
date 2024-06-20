6e480d36e06d15616781d780d4bc7627c93adb6c
jowatson2011 <jowatson@protonmail.com>
2024-06-20 09:45:59 +0100

    Added event handler that deletes comment on button click

--- src/components/CommentCards.jsx
+++ src/components/CommentCards.jsx
@@ -1,24 +1,46 @@
 import { useContext } from "react";
 import LikeButton from "./LikeButton";
 import { UserContext } from "../contexts/UserContext";
+import { deleteRequest } from "../utils/api";
 
-function CommentCard({ comment, setComments }) {
+function CommentCard({ comment, setComments, setDeleteSuccessful }) {
   const { user } = useContext(UserContext);
 
+  const handleDeleteComment = (e) => {
+    const comment_id = e.target.value;
+    deleteRequest(`/api/comments/${comment_id}`, {
+      params: { comment_id },
+    }).then(() => {
+      setComments((currComments) => {
+        setDeleteSuccessful(true);
+        console.log(comment_id, "deleted!");
+        const newArr = [...currComments];
+        return newArr.filter((comment) => {
+          comment.comment_id != comment_id;
+        });
+      });
+    });
+  };
+
   return (
-    <div className=" m-10 grid grid-rows-2 grid-cols-3 border-s-black border p-2">
+    <div className=" grid grid-rows-2 grid-cols-3 border-s-black border p-2 rounded">
       <h4 className=" font-bold">{comment.author}</h4>
       <p className=" italic" data-cy="date">
         {comment.created_at.slice(0, 10)}
       </p>
-      <LikeButton
-        className=" row-span-2"
-        postWithVotes={comment}
-        setFunction={setComments}
-      />
+      <LikeButton postWithVotes={comment} setFunction={setComments} />
+
       <p className=" col-span-2" data-cy="body">
         {comment.body}
       </p>
+      <button
+        type="button"
+        onClick={handleDeleteComment}
+        className=" hover:bg-red-900 rounded-full border"
+        value={comment.comment_id}
+      >
+        Delete comment
+      </button>
     </div>
   );
 }
