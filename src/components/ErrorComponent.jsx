8dbd871043c9ad7185c4b3fc297c87864534de90
jowatson2011 <jowatson@protonmail.com>
2024-06-20 16:15:27 +0100

    Add formatting to ErrorComponent for 404 errors

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,4 +1,14 @@
-const ErrorComponent = () => {
-  return <p>Something went wrong</p>;
+const ErrorComponent = ({ error }) => {
+    console.log(error.response.status)
+  return (
+    <div className="m-10">
+      <p>Something went wrong!</p>
+      {error.response.status === 404 ? (
+        <p>
+          Check the <span className=" text-red-700">{error.response.data.msg.slice(10)}</span> query
+        </p>
+      ) : null}
+    </div>
+  );
 };
 export default ErrorComponent;
