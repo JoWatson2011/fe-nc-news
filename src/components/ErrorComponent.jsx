93c996de2e03cddb134047e38c819fb9acb48050
jowatson2011 <jowatson@protonmail.com>
2024-06-20 16:50:17 +0100

    Added content for 404 and 400 erors

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,13 +1,18 @@
 const ErrorComponent = ({ error }) => {
-    console.log(error.response.status)
+  console.log(error.response.status);
   return (
     <div className="m-10">
       <p>Something went wrong!</p>
-      {error.response.status === 404 ? (
+      {error.response.status === 400 ? (
         <p>
-          Check the <span className=" text-red-700">{error.response.data.msg.slice(10)}</span> query
+          Check the{" "}
+          <span className=" text-red-700">
+            {error.response.data.msg.slice(12)}
+          </span>{" "}
+          query
         </p>
       ) : null}
+      {error.response.status === 404 ? <p>Page not found</p> : null}
     </div>
   );
 };
