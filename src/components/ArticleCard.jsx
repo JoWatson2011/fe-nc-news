851fba7a9b13f6dd398b4632419c0e09b23c47b4
jowatson2011 <jowatson@protonmail.com>
2024-06-21 16:39:41 +0100

    Corrected navigation to other pages from links in article cards

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -15,19 +15,27 @@ const ArticleCard = ({ article, setArticle }) => {
     <div>
       <div className="flex flex-row w-full mt-5 mb-5">
         <div className="mr-10">
-          <img src={article.article_img_url} className="article-image" />
-          <LikeButton postWithVotes={article} setFunction={setArticle} />
-        </div>
-        <div>
-          <h3 className="font-mono font-semibold">{article.title}</h3>
-          <p
+          <img
             onClick={() => {
               navigate(`/articles/${article.article_id}`);
             }}
-            className="font-mono text-red-900"
+            src={article.article_img_url}
+            className="article-image"
+          />
+          <LikeButton postWithVotes={article} setFunction={setArticle} />
+        </div>
+        <div>
+          <h3
+            className="font-mono font-semibold"
+            onClick={() => {
+              navigate(`/articles/${article.article_id}`);
+            }}
           >
-            {article.topic}
-          </p>
+            {article.title}
+          </h3>
+          <Link to={`/articles?topic=${article.topic}`} reloadDocument="true">
+            <p className="font-mono text-red-900">{article.topic}</p>
+          </Link>
           <p className="font-mono">{article.created_at.slice(0, 10)}</p>
           <p className="font-mono">{article.comment_count} comments</p>
         </div>
