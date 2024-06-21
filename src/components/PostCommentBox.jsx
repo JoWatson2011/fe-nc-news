83d4ad31e31fe74cc450c68cbb88236f23fa3caf
jowatson2011 <jowatson@protonmail.com>
2024-06-21 09:48:09 +0100

    Added error handling for empty comment box

--- src/components/PostCommentBox.jsx
+++ src/components/PostCommentBox.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import { postRequest } from "../utils/api";
@@ -11,6 +11,8 @@ const PostCommentBox = ({
   const { article_id } = useParams();
 
   const { user } = useContext(UserContext);
+
+  const [error, setError] = useState(null);
 
   const [commentFocus, setCommentFocus] = useState(false);
   const [commentBoxSize, setCommentBoxSize] = useState("1");
@@ -25,11 +27,17 @@ const PostCommentBox = ({
     } else {
       setCommentBoxSize("1");
       setCommentBoxStyle("p-1 border border-gray-700 rounded-full");
+      setPostSuccessful(0);
     }
   }, [commentFocus]);
 
   const handlePostComment = (e) => {
     e.preventDefault();
+
+    if (!newComment) {
+      setError("Please enter a comment");
+      return;
+    }
 
     postRequest(`/api/articles/${article_id}/comments`, {
       body: newComment,
@@ -38,6 +46,7 @@ const PostCommentBox = ({
       params: { article_id },
     })
       .then(() => {
+        setError(null);
         setPostSuccessful((currValue) => {
           return currValue + 1;
         });
@@ -45,46 +54,51 @@ const PostCommentBox = ({
       .then(() => {
         setNewComment("");
       })
+      .then(() => {
+        setCommentFocus(false);
+      })
       .catch((err) => {
-        console.log(err);
-        setPostSuccessful(false);
+        setError("Something went wrong, please try again");
+        setPostSuccessful(0);
       });
   };
 
   return (
-    <form className="grid grid-cols-1 m-10" onSubmit={handlePostComment}>
-      <label className="grid grid-cols-1">
-        Post a comment
-        <textarea
-          rows={commentBoxSize}
-          cols="50"
-          placeholder="Add a comment"
+    <div>
+      {postSuccessful ? <p>Comment posted</p> : null}
+      <form className="grid grid-cols-1 m-10" onSubmit={handlePostComment}>
+        <label className="grid grid-cols-1">
+          Post a comment
+          <textarea
+            rows={commentBoxSize}
+            cols="50"
+            placeholder="Add a comment"
+            onFocus={() => {
+              setCommentFocus(true);
+            }}
+            onBlur={() => {
+              setCommentFocus(false);
+            }}
+            className={commentBoxStyle}
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+        </label>
+        <button
+          type="submit"
           onFocus={() => {
             setCommentFocus(true);
           }}
           onBlur={() => {
             setCommentFocus(false);
           }}
-          className={commentBoxStyle}
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          required
-        />
-      </label>
-      <button
-        type="submit"
-        onFocus={() => {
-          setCommentFocus(true);
-        }}
-        onBlur={() => {
-          setCommentFocus(false);
-        }}
-        className=" m-2 p-2 bg-red-50 rounded-full"
-      >
-        Post Comment
-      </button>
-      {postSuccessful ? <p>Comment posted</p> : null}
-    </form>
+          className=" m-2 p-2 bg-red-50 rounded-full"
+        >
+          Post Comment
+        </button>
+        {error ? <p>{error}</p> : null}
+      </form>
+    </div>
   );
 };
 export default PostCommentBox;
