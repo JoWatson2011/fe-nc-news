170ff23850b379d7ac393fe7c559c20fb5d24b06
jowatson2011 <jowatson@protonmail.com>
2024-07-24 17:02:42 +0100

    Passed icons down on props to NavButtons

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import { useContext } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import FaceIcon from "@mui/icons-material/Face";
+import LoginIcon from "@mui/icons-material/Login";
 import NavButton from "./NavButton";
 const NavMenu = () => {
   const { user } = useContext(UserContext);
@@ -10,15 +13,15 @@ const NavMenu = () => {
       {user ? (
         <div className="flex">
           <Link to="/articles/post">
-            <NavButton buttonText={"Post Article"} />
+            <NavButton buttonText={"Post Article"} icon={<AddIcon />} />
           </Link>
           <Link to="/account">
-            <NavButton buttonText={"My Account"} />
+            <NavButton buttonText={"My Account"} icon={<FaceIcon />} />
           </Link>
         </div>
       ) : (
         <Link to="login">
-          <NavButton buttonText={"Log In"} />
+          <NavButton buttonText={"Log In"} icon={<LoginIcon />} />
         </Link>
       )}
     </div>
