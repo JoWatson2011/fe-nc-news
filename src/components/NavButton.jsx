2fe7942d421fa869ca9c227d1030fd73cd67f16e
jowatson2011 <jowatson@protonmail.com>
2024-07-25 11:58:43 +0100

    Formatted by prettier

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,14 +1,14 @@
 const NavButton = ({ buttonText, handleClick, icon }) => {
   return (
-      <button
-        data-cy="button"
-        type="button"
-        onClick={handleClick}
-        className=" p-2 bg-red-50 rounded-full text-nowrap"
-      >
-        {icon ? icon : null}
-        {buttonText}
-      </button>
+    <button
+      data-cy="button"
+      type="button"
+      onClick={handleClick}
+      className=" p-2 bg-red-50 rounded-full text-nowrap"
+    >
+      {icon ? icon : null}
+      {buttonText}
+    </button>
   );
 };
 
