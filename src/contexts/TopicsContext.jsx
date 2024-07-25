82105bf3c878fa98484c4da3555f4d01baabcd93
jowatson2011 <jowatson@protonmail.com>
2024-07-25 11:40:22 +0100

    Create new context for article topics

---
+++ src/contexts/TopicsContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+import { getRequest } from "../utils/api";
+export const TopicsContext = createContext();
+
+export const TopicsProvider = ({ children }) => {
+  const [topics, setTopics] = useState([]);
+  const [awaitingTopics, setAwaitingTopics] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { topics } = await getRequest("/api/topics/");
+        setTopics(topics);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setAwaitingTopics(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <TopicsContext.Provider value={{ topics, awaitingTopics }}>
+      {children}
+    </TopicsContext.Provider>
+  );
+};
