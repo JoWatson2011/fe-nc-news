874d1bbd4e9645f12504cdd88418b0764aea8330
jowatson2011 <jowatson@protonmail.com>
2024-07-26 11:31:02 +0100

    Added a welcome message on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,15 +2,12 @@ import ArticleCard from "./ArticleCard";
 import NavButton from "./NavButton";
 import Loading from "./Loading";
 import { getRequest } from "../utils/api";
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../contexts/UserContext";
 import { Link } from "react-router-dom";
-const Home = ({
-  listArticles,
-  setlistArticles,
-  isLoading,
-  setIsLoading,
-  setArticle,
-}) => {
+const Home = ({ listArticles, setlistArticles, isLoading, setIsLoading }) => {
+  const { userDetails } = useContext(UserContext);
+
   const months = [
     "January",
     "February",
@@ -36,9 +33,19 @@ const Home = ({
 
   return (
     <div className="ml-10">
-      <h2 className="font-mono text-[30px]">{`${date.getDate()} ${
+      <div className="flex items-center mb-10">
+        <img
+          src={userDetails.avatar_url}
+          className="rounded-full border-2 border-red-800 h-[70px] mr-5"
+        ></img>
+        <h2 className="font-mono text-[30px]">
+          Welcome back{" "}
+          <span className="text-red-800 ">{userDetails.username}!</span>
+        </h2>
+      </div>
+      <h3 className="font-mono text-[30px]">{`${date.getDate()} ${
         months[date.getMonth()]
-      } ${date.getFullYear()}`}</h2>
+      } ${date.getFullYear()}`}</h3>
       <p className="font-mono mb-5">Today's top articles</p>
       <div className="grid-flow-col  space-y-8">
         {isLoading ? <Loading /> : null}
