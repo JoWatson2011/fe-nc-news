7c0162003eb7854288ee00e66d690e928d402ad1
jowatson2011 <jowatson@protonmail.com>
2024-07-29 11:04:47 +0100

    Removed hard coded user from context

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -3,13 +3,8 @@ import { useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState("jessjelly");
-  const [userDetails, setUserDetails] = useState({
-    username: "jessjelly",
-    name: "Jess Jelly",
-    avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
-  });
+  const [user, setUser] = useState("");
+  const [userDetails, setUserDetails] = useState({});
 
   return (
     <UserContext.Provider value={{ user, userDetails }}>
