6be2e8395fbf992a436247a523bc8e9e288d3df0
jowatson2011 <jowatson@protonmail.com>
2024-07-29 14:39:11 +0100

    Added a log out button that uses UserDispatch function and naviagtes to root

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -1,5 +1,28 @@
+import Button from "@mui/material/Button";
+import { useContext } from "react";
+import { UserDispatchContext } from "../contexts/UserContext";
+import { useNavigate } from "react-router-dom";
+
 const MyAccount = () => {
-    return (<p>My Account page</p>)
+  const dispatch = useContext(UserDispatchContext);
+  const navigate = useNavigate();
+  return (
+    <main className="flex flex-col w-full place-items-center space-y-5">
+      <Button
+        sx={{
+          borderRadius: "30px",
+          color: "darkred",
+          border: "1px solid darkred",
+        }}
+        onClick={() => {
+          dispatch({ type: "logout" });
+          navigate("/");
+        }}
+      >
+        Log Out
+      </Button>
+    </main>
+  );
 };
 
 export default MyAccount;
