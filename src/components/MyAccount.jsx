62f394e75da4d8dcf8202a4dac1402d6bdb9c1bc
jowatson2011 <jowatson@protonmail.com>
2024-07-29 15:08:05 +0100

    Displayed users articles on MyAccount page

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -1,11 +1,26 @@
 import Button from "@mui/material/Button";
-import { useContext } from "react";
-import { UserDispatchContext } from "../contexts/UserContext";
+import { useContext, useEffect, useState } from "react";
+import { UserDispatchContext, UserContext } from "../contexts/UserContext";
 import { useNavigate } from "react-router-dom";
+import { getRequest } from "../utils/api";
+import ArticleCard from "./ArticleCard";
 
 const MyAccount = () => {
   const dispatch = useContext(UserDispatchContext);
+  const { userDetails } = useContext(UserContext);
   const navigate = useNavigate();
+
+  const [userArticles, setUserArticles] = useState([]);
+
+  useEffect(() => {
+    getRequest("/api/articles").then(({ articles }) => {
+      const userArticles = articles.filter(
+        (article) => article.author === userDetails.username
+      );
+      setUserArticles(userArticles);
+    });
+  }, []);
+
   return (
     <main className="flex flex-col w-full place-items-center space-y-5">
       <Button
@@ -21,6 +36,22 @@ const MyAccount = () => {
       >
         Log Out
       </Button>
+      <div className="flex flex-col items-center">
+        <img
+          src={userDetails.avatar_url}
+          className="rounded-full border-2 border-red-800 h-[70px] mr-5"
+        />
+        <h2 className="text-[30px] font-mono ">{userDetails.name}</h2>
+      </div>
+
+      <div>
+        <h3 className="text-[20px] font-mono ">Your articles</h3>
+        <section className=" border border-grey p-5 rounded-[20px]">
+          {userArticles.map((article) => {
+            return <ArticleCard article={article} />;
+          })}
+        </section>
+      </div>
     </main>
   );
 };
