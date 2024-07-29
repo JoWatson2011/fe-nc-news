c19a16eb1fa2c471a3a0c5ccaef25d1f0e7a7ba3
jowatson2011 <jowatson@protonmail.com>
2024-07-29 15:53:50 +0100

    Added conditional styling if button id matches url path

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,10 +1,21 @@
-const NavButton = ({ buttonText, handleClick, icon }) => {
+import { useLocation } from "react-router-dom";
+
+const NavButton = ({ buttonText, handleClick, icon, id }) => {
+  const { pathname } = useLocation();
+
+  const regex = new RegExp(`${id}$`);
+  const urlMatches = !!pathname.match(regex);
+
+  let buttonStyling = "p-2 rounded-full text-nowrap hover:bg-red-200";
+  buttonStyling += urlMatches ? " bg-red-200 text-semibold" : " bg-red-50";
+
   return (
     <button
+      id={id}
       data-cy="button"
       type="button"
       onClick={handleClick}
-      className=" p-2 bg-red-50 rounded-full text-nowrap"
+      className={buttonStyling}
     >
       {icon ? icon : null}
       {buttonText}
